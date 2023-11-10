3167abb7416eae3167ecaad1723dfac3620672a2
Sandeep Puthanveetil Satheesan <sandeeps@illinois.edu>
2023-11-10 15:49:37 -0600

    Add code for debugging.

--- 03_NDPI_Slide_Annotation/03_00_ndpi_cropping.py
+++ 03_NDPI_Slide_Annotation/03_00_ndpi_cropping.py
@@ -9,14 +9,21 @@ import bioformats.formatreader as format_reader
 import javabridge
 import numpy as np
 from PIL import Image
+from bioformats import ImageReader
 
 javabridge.start_vm(class_path=bioformats.JARS, max_heap_size="4G", run_headless=True)
 
 
 def img_read(image_filepath,  x=0, y=0, z=0, width=1000, height=1000):
+    print("A")
+    image_reader = format_reader.make_image_reader_class()
+    print("B")
     reader = image_reader()
+    print("C")
     reader.setId(image_filepath)
+    print("D")
     image = reader.openBytesXYWH(z, x, y, width, height)
+    print("E")
     image.shape = (height, width, 3)
 
     return image
@@ -40,10 +47,11 @@ metadata.update({'width': width})
 metadata.update({'height': height})
 metadata.update({'zPlane': zPlane})
 
-image_reader = format_reader.make_image_reader_class()
-reader = image_reader()
-reader.setId(img_path)
-image = reader.openBytesXYWH(zPlane, 0, 0, width, height)
+# image_reader = format_reader.make_image_reader_class()
+# reader = image_reader()
+# reader.setId(img_path)
+# image = reader.openBytesXYWH(zPlane, 0, 0, width, height)
+# image = None
 
 img_name = os.path.basename(img_path).split(' ')[0]
 core_name = img_path.split('/')[-2].split('_')[0]
@@ -67,27 +75,32 @@ for i in range(len(start_x_list)):
         start_xy_list.append(xy)
 
 
-def crop_image(i, start_xy_list, zPlane, crops_dir, img_path, image, width, height):
+def crop_image(i, start_xy_list, zPlane, crops_dir, img_path, width, height):
     start_x = start_xy_list[i][0]
     start_y = start_xy_list[i][1]
+    print("Processing coordinates: ", start_y, start_y)
     tile_dir = os.path.join(crops_dir, str(start_x) + 'x_' + str(start_y) + 'y')
     if not os.path.exists(tile_dir):
         os.makedirs(tile_dir)
     # print(img_path)
     for j in range(zPlane):
-        # img = img_read(img_path, x=start_x, y=start_y, z=j, width=width, height=height)
-        image.shape = (height, width, 3)
-        im = Image.fromarray(image)
+        print("Processing layer:", i, j)
+        img = img_read(img_path, x=start_x, y=start_y, z=j, width=width, height=height)
+        print("Read image")
+        img.shape = (height, width, 3)
+        im = Image.fromarray(img)
+        print("Obtained image data")
         im.save(os.path.join(tile_dir, str(j) + 'z.png'))
+        print("Save layer", i, j)
     return i
 
 
-# index_list = range(len(start_xy_list))
-index_list = [0, 1, 2, 3, 4, 5, 6, 7]
+index_list = range(len(start_xy_list))
+# index_list = [0, 1, 2, 3, 4, 5, 6, 7]
 
 
 with multiprocessing.Pool(processes=4) as pool:
-    results = [pool.apply(crop_image, args=(i, start_xy_list, zPlane, crops_dir, img_path, image, width, height)) for i in
+    results = [pool.apply(crop_image, args=(i, start_xy_list, zPlane, crops_dir, img_path, width, height)) for i in
                index_list]
 
 # for i in range(len(start_xy_list)):
